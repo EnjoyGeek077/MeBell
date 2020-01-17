3de745d926ea5614c271c58f742aa5277d6b2bb5
fedegar99 <federicogargiulo1999@gmail.com>
2020-01-17 17:39:44 +0100

    Revert "Inizializzazione progetto java"
    This reverts commit bcf2ef9fda62a7ecdff8744324b00b6bdfcbb5e5.

--- Java/src/main/Controller.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public class Controller {
-	private StudenteDao stDao;
-}
--- Java/src/main/StudenteDao.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-import java.util.List;
-
-public interface StudenteDao {
-	public List<Studente> getAllStudent();
-}
