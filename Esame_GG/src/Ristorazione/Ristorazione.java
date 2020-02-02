d60e61c37868e44d4c62ec4219979420dab79b81
fedegar99 <federicogargiulo1999@gmail.com>
2020-02-02 19:27:25 +0100

    Update Ristorazione

--- Esame_GG/src/Ristorazione/Ristorazione.java
+++ Esame_GG/src/Ristorazione/Ristorazione.java
@@ -3,14 +3,14 @@ package Ristorazione;
 import Entità.Location;
 
 public class Ristorazione extends Location{
-	
-    private int prezzoMedio;
-    private int nPosti;
-    private String tipoRistorazione;
-    
-    private SushiBar sushiBar; 
-    private Pizzeria pizzeria;
-    private Braceria braceria;
+
+	private int prezzoMedio;
+	private int nPosti;
+	private String tipoRistorazione;
+
+	private SushiBar sushiBar; 
+	private Pizzeria pizzeria;
+	private Braceria braceria;
 
 	public Ristorazione(Location mainLoc, int prezzomedio, int nPosti, String tipoRistorazione) {
 		super(mainLoc.getCod(), mainLoc.getNome(), mainLoc.getCod_residenza(), mainLoc.getTipo(), mainLoc.getPartitaIva(), mainLoc.getDescrizione(), mainLoc.getResidenzaLocation(), mainLoc.getRecensiondiLocation());
@@ -18,45 +18,58 @@ public class Ristorazione extends Location{
 		this.prezzoMedio=prezzomedio;
 		this.nPosti=nPosti;
 		this.tipoRistorazione=tipoRistorazione;
-	    }
-	
-	
-
-    @Override
-	public String toString() {
-	    String infoBraceria="Informazioni Braceria:\nAsporto: "+this.braceria.isAsporto()+", Serve alcolici artigianali: "+this.braceria.isAlcoliciArtigianali()+", Tipo Carne: "+this.braceria.getTipoCarne()+"\n";
-	    String infoPizzeria="Informazioni Pizzeria:\nAsporto: "+this.pizzeria.isAsporto()+", Tipo Forno: "+this.pizzeria.getTipoForno()+"\n";
-	    String infoSushiBar="Informazioni SushiBar:\nAllYouCanEat: "+this.sushiBar.isAllYouCanEat()+", Tipo sushi: "+this.sushiBar.getTipoSushi()+"\n";
-	   
-	    String StringaReturn="";
-	    
-	    
-	    StringaReturn=StringaReturn.replace("true", "si");
-	    StringaReturn=StringaReturn.replace("false", "no");
-	    
-	    return StringaReturn;
 	}
 
 
 
-    public int getPrezzoMedio() {
-	return prezzoMedio;
-    }
-    public void setPrezzoMedio(int prezzoMedio) {
-	this.prezzoMedio = prezzoMedio;
-    }
-    public int getnPosti() {
-	return nPosti;
-    }
-    public void setnPosti(int nPosti) {
-	this.nPosti = nPosti;
-    }
-    public String getTipoRistorazione() {
-	return tipoRistorazione;
-    }
-    public void setTipoRistorazione(String tipoRistorazione) {
-	this.tipoRistorazione = tipoRistorazione;
-    }
+	@Override
+	public String toString() {
+
+
+		String StringaReturn="";
+
+		if(this.sushiBar!=null) {
+			String infoSushiBar="Informazioni SushiBar:\nAllYouCanEat: "+this.sushiBar.isAllYouCanEat()+", Tipo sushi: "+this.sushiBar.getTipoSushi()+"\n\n";
+			StringaReturn=StringaReturn+infoSushiBar;
+		}
+		if(this.pizzeria!=null) {
+			String infoPizzeria="Informazioni Pizzeria:\nAsporto: "+this.pizzeria.isAsporto()+", Tipo Forno: "+this.pizzeria.getTipoForno().replace("Entrambi", "Elettrico e A legna")+"\n\n";
+			StringaReturn=StringaReturn+infoPizzeria;
+		}
+		if(this.braceria!=null) {
+			String infoBraceria="Informazioni Braceria:\nAsporto: "+this.braceria.isAsporto()+", Serve alcolici artigianali: "+this.braceria.isAlcoliciArtigianali()+", Tipo Carne: "+this.braceria.getTipoCarne().replace("Entrambe", "Italiana e Importata")+"\n\n";
+			StringaReturn=StringaReturn+infoBraceria;
+		}
+
+
+
+
+		StringaReturn=StringaReturn.replace("true", "si");
+		StringaReturn=StringaReturn.replace("false", "no");
+
+		return StringaReturn;
+	}
+
+
+
+	public int getPrezzoMedio() {
+		return prezzoMedio;
+	}
+	public void setPrezzoMedio(int prezzoMedio) {
+		this.prezzoMedio = prezzoMedio;
+	}
+	public int getnPosti() {
+		return nPosti;
+	}
+	public void setnPosti(int nPosti) {
+		this.nPosti = nPosti;
+	}
+	public String getTipoRistorazione() {
+		return tipoRistorazione;
+	}
+	public void setTipoRistorazione(String tipoRistorazione) {
+		this.tipoRistorazione = tipoRistorazione;
+	}
 	public SushiBar getSushiBar() {
 		return sushiBar;
 	}
