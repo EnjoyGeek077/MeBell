94aa2b0570c7eac1ce2c314b9914858cc20effb9
Antonio Garofalo <antonio.garofalo125@gmail.com>
2022-04-05 16:09:33 +0200

    Varie revisioni per GitHub.

--- Esame_GG/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"Alloggio"},{"l":"All Packages","url":"allpackages-index.html"},{"l":"Attrazione"},{"l":"Connessione"},{"l":"Controller"},{"l":"DAO"},{"l":"Entita"},{"l":"Exception"},{"l":"Ristorazione"},{"l":"Swing"},{"l":"SwingDialog"},{"l":"Utility"}]
--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Federico Gargiulo - N86002884
 Antonio Garofalo - N86003129
 ```
 
-Utilizzata versione Java: 11.0.5 LTS (aggiornata a 11.0.10).
+Primo progetto con tecnologia Database SQL e Java.
